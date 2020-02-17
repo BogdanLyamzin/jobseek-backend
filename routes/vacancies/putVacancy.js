b30c8792d6550f263e8f4aa1e998abe8cae1feda
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-02-17 14:09:43 +0200

    fix upload img and change update routes

--- routes/vacancies/putVacancy.js
+++ routes/vacancies/putVacancy.js
@@ -1,21 +1,22 @@
 const Vacancy = require("../../models/Vacancy");
 
-module.exports = (app) => {
-    app.put('/vacancies/:id', async (req, res) => {
-        
-        try {
-            const result = await Vacancy.findByIdAndUpdate(req.params.id, req.body);
-            res.send({
-                status: "Success",
-                result: result,
-            });                
-        } catch(err) {
-            res.send({
-                status: "Error",
-                message: err,
-            });           
-        }         
-        /*
+module.exports = app => {
+  app.put("/vacancies/:id", async (req, res) => {
+    try {
+      const result = await Vacancy.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+      });
+      res.send({
+        status: "Success",
+        result: result
+      });
+    } catch (err) {
+      res.send({
+        status: "Error",
+        message: err
+      });
+    }
+    /*
         const updateVacancy = await Vacancy.findByIdAndUpdate(req.params.id, req.body, (err, update) => {
             // mongoose.disconnect();
             if (err) return res.send({
@@ -30,5 +31,5 @@ module.exports = (app) => {
             result: updateVacancy,
         });
         */
-    });
+  });
 };
