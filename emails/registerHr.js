f962e7e468fcfa95861eeed193f20fa998f60234
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-03-17 16:28:15 +0200

    change getHR

--- emails/registerHr.js
+++ emails/registerHr.js
@@ -8,7 +8,7 @@ module.exports = (email, password) => ({
     <h1>Вітаємо на Jobseek!</h1>
     <p>Ви успішно створили профіль.</p>
     <p>Дані для входу:</p>
-    <p>Email: ${email};</p>
+    <p>Email: ${email}</p>
     <p>Password: ${password}</p>
     `
 });
